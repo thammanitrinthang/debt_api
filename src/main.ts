f334a2ae5e10656620e602304541bc387c6b4282
THAMMANIT RINTHANG <6431503031@lamduan.mfu.ac.th>
2024-05-19 16:48:02 +0700

    edit

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cors from 'cors';
 import * as cookieParser from "cookie-parser";
-import { hostname } from 'os';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,6 +11,6 @@ async function bootstrap() {
       credentials: true
   })
 
-  await app.listen(process.env.PORT, '0.0.0.0');
+  await app.listen(8000);
 }
 bootstrap();
