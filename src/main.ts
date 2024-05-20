91d9016d019d30de0768bc7cb059bb90602d93c4
THAMMANIT RINTHANG <6431503031@lamduan.mfu.ac.th>
2024-05-20 14:56:44 +0700

    update

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,20 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import * as cookieParser from 'cookie-parser';
+  import { NestFactory } from '@nestjs/core';
+  import { AppModule } from './app.module';
+  import * as cookieParser from 'cookie-parser';
 
-async function bootstrap() {
-  try {
-    const app = await NestFactory.create(AppModule);
-    app.use(cookieParser());
-    app.enableCors({
-      origin: 'http://localhost:3000',
-      credentials: true,
-    });
+  async function bootstrap() {
+    try {
+      const app = await NestFactory.create(AppModule);
+      app.use(cookieParser());
+      app.enableCors({
+        origin: 'http://localhost:3000',
+        credentials: true,
+      });
 
-    await app.listen(8000);
-    console.log('Application is running on: http://localhost:8000');
-  } catch (error) {
-    console.error('Error during application bootstrap', error);
+      await app.listen(8000);
+      console.log('Application is running on: http://localhost:8000');
+    } catch (error) {
+      console.error('Error during application bootstrap', error);
+    }
   }
-}
-bootstrap();
+  bootstrap();
