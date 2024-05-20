15841d8418c10e4bbb22ce9d7536d9d03a3236e1
THAMMANIT RINTHANG <6431503031@lamduan.mfu.ac.th>
2024-05-20 14:52:25 +0700

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -36,7 +36,7 @@ dotenv.config();
     TypeOrmModule.forFeature([User]),
     JwtModule.register({
       secret: process.env.JWT_SECRET,
-      signOptions: { expiresIn: '1d' }
+      signOptions: { expiresIn: 'id' }
     }),
   ],
   controllers: [AppController],
